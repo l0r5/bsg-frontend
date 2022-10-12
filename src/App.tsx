d45db86a0cd9a5104cbfbc8f9c5d42c9850e6274
Lars Braitinger <Lars.Braitinger@netcetera.com>
2022-10-12 20:35:50 +0200

    Add an input to create todos and add project related files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.css';
-import Todos from "./components/Todos";
-import Todo from './models/todo'
+import Todos from './components/Todos';
+import NewTodo from './components/NewTodo';
+import {useState} from 'react';
+import Todo from './models/todo';
 
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+        const newTodo = new Todo(text);
+
+        setTodos((prevState => {
+          return prevState.concat(newTodo);
+        }));
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
     </div>
   );
