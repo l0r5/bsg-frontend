8022f60acf15ab3e66418f32ab457b3ae402b3be
Lars Braitinger <Lars.Braitinger@netcetera.com>
2022-10-12 18:32:34 +0200

    Create stupid todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css';
+import Todos from "./components/Todos";
+import Todo from './models/todo'
+
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+  ];
+
   return (
-    <div className="App">
+    <div>
+      <Todos items={todos}/>
     </div>
   );
 }
